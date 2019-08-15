4074e76909185663eb33841310fb75c6c1a22aaf
Tanya Bihag <tbihag@gmail.com>
2019-08-15 04:58:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,22 +3,25 @@ def using_include(array, element)
 end
 
 def using_sort(array)
+  using_sort!sort
   
 
 end
 
 def using_reverse(array)
+  using_reverse!reverse
 
 end
 
 def using_first(array)
+  using_first.first
 
 end
 
 def using_last(array)
-
+  using_last.last
 end
 
 def using_size(array)
-
+  using_size.size?
 end
