772fdfb200f249312930bfec3eafc66dc129df58
Tanya Bihag <tbihag@gmail.com>
2019-08-15 05:00:19 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,24 +1,24 @@
-def using_include(array, element)
+def using_include[array , hi]
   using_include.include?(element)
 end
 
-def using_sort(array)
+def using_sort[array , hi]
   using_sort!sort
   
 
 end
 
-def using_reverse(array)
+def using_reverse[array , hi]
   using_reverse!reverse
 
 end
 
-def using_first(array)
+def using_first[array , hi]
   using_first.first
 
 end
 
-def using_last(array)
+def using_last[array , hi]
   using_last.last
 end
 
